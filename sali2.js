5658f1a966c8bf8465df55ffeb5767d9c938d3e3
hasanyilmaz61 <83864802+hasanyilmaz61@users.noreply.github.com>
2021-08-01 21:11:11 +0200

    erstellt

---
+++ sali2.js
@@ -0,0 +1,26 @@
+let arkaplan=document.getElementById("hintegrund");
+let uberuns=document.getElementById("uberuns");
+let bild4=document.getElementById("bild4");
+function uber() {
+
+   
+     arkaplan.style.display="none";
+     uberuns.style.display="block";  
+     kontakt.style.display="none";
+      
+    
+}
+
+function home() {
+    arkaplan.style.display="block";
+     uberuns.style.display="none";  
+     kontakt.style.display="none";
+}
+
+
+function aut() {
+     
+     arkaplan.style.display="block";
+     uberuns.style.display="none";  
+     kontakt.style.display="none"; 
+}
---
+++ sali4.js
@@ -0,0 +1,11 @@
+let kontakt=document.getElementById("kontakt")
+
+
+
+function kontak() {
+
+    kontakt.style.display="block";
+    arkaplan.style.display="none";
+    uberuns.style.display="none";  
+}
+
